8866dc7ac61a1c4568829ef856e2ee3d7ee32231
lewis-r-white <lewisrwhite@gmail.com>
2023-11-15 14:41:46 -0800

    get downloading by date for multiple devices to work

--- get_full_data_test.R
+++ get_full_data_test.R
@@ -1,26 +1,12 @@
-#' Get data for devices from Quant AQ
-#'
-#' @param start_date character, yyyy-mm-dd
-#' @param end_date character, yyyy-mm-dd
-#' @param device_names list of characters,
-#'
-#' @return dataframe with observations for each device between the date range of interest 
-#' @export
-#'
-#' @examples
-#' 
-#' start_date <- as.Date("2023-10-15")
-#' end_date <- as.Date("2023-11-12")
-#' device_names <- c("MOD-PM-00826", "MOD-PM-00847")
-#' 
-#' result_df <- get_data_for_devices(start_date, end_date, device_names)
-
-
 # Function to get data by date, handling errors
 get_data_safe <- possibly(get_data_by_date, otherwise = NULL)
 
-# Function to get data for each device within the dates specfied
-get_data_for_devices <- function(start_date, end_date, device_names) {
+# Function to get data for each device within the dates specified
+get_full_data_test <- function(start_date, end_date, device_names) {
+  
+  # Convert start_date and end_date to POSIXt objects
+  start_date <- as.POSIXct(start_date)
+  end_date <- as.POSIXct(end_date)
   
   # Use map to get data for each device and date, handling errors
   result_df <- map_df(device_names, function(device) {
@@ -35,17 +21,15 @@ get_data_for_devices <- function(start_date, end_date, device_names) {
     
     # Combine the list of data frames into a single data frame
     result_df_device <- do.call(rbind, result_list) %>%
-      
-      #fill in the missing time gaps
+      # Fill in the missing time gaps
       mutate(timestamp = as.POSIXct(timestamp)) %>%
       mutate(timestamp = format(timestamp, "%Y-%m-%d %H:%M")) %>%
       mutate(timestamp2 = as.POSIXct(timestamp)) %>%
-      complete(timestamp2 = seq.POSIXt(min(timestamp2), max(timestamp2), by = "min")) %>%
+      complete(timestamp2 = seq.POSIXt(as.POSIXct(start_date), as.POSIXct(end_date), by = "min")) %>%
       mutate(date = as.Date(timestamp2)) %>%
       mutate(hour = hour(ymd_hms(timestamp2))) %>%
       mutate(monitor = device) %>%
       select(monitor, everything())
-      
     
     return(result_df_device)
   })
@@ -53,4 +37,8 @@ get_data_for_devices <- function(start_date, end_date, device_names) {
   return(result_df)
 }
 
-
+# # Example usage
+# start_date <- as.Date("2023-10-15")
+# end_date <- as.Date("2023-11-12")
+# device_names <- c("MOD-PM-00847", "other_device_name")
+# result <- get_data_for_devices(start_date, end_date, device_names)
