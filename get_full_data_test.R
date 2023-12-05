af3da3f2db61c349958c1a76908a4d276d41b0ee
lewis-r-white <lewisrwhite@gmail.com>
2023-12-05 15:23:08 -0800

    Initial adjustment to handle error from offline devices

--- get_full_data_test.R
+++ get_full_data_test.R
@@ -1,35 +1,39 @@
-# Function to get data by date, handling errors
+# Wrap the function `get_data_by_date` within `possibly` to handle potential errors
 get_data_safe <- possibly(get_data_by_date, otherwise = NULL)
 
-# Function to get data for each device within the dates specified
+
+# Define a function `get_full_data_test` that takes start date, end date, and device names as inputs
 get_full_data_test <- function(start_date, end_date, device_names) {
   
-  # Convert start_date and end_date to POSIXt objects
+  # Convert start_date and end_date to POSIXt objects for date manipulation
   start_date <- as.POSIXct(start_date)
   end_date <- as.POSIXct(end_date)
   
-  # Use map to get data for each device and date, handling errors
+  # Use `map_df` to iterate over each device in `device_names` and retrieve data for each day
   result_df <- map_df(device_names, function(device) {
+    
+    # Use `map` to iterate through each day between `start_date` and `end_date`
     result_list <- map(seq(start_date, end_date, by = "days"), function(date) {
-      formatted_date <- format(date, "%Y-%m-%d")
-      get_data_safe(sn = device, date = formatted_date)
+      formatted_date <- format(date, "%Y-%m-%d") #format date
+      get_data_safe(sn = device, date = formatted_date) # Call the `get_data_safe` function with device serial number (`sn`) and formatted date
     }) %>%
-      # Filter out NULL elements (empty lists)
+      
+      # Filter out NULL elements (empty lists) or lists with length 0 (no data)
       discard(~ is.null(.x) || length(.x) == 0) %>%
       # Convert each element to a data frame
       map(as.data.frame)
-    
-    # Combine the list of data frames into a single data frame
+    # Combine the list of data frames into a single data frame for each device 
     result_df_device <- do.call(rbind, result_list) %>%
-      # Fill in the missing time gaps
-      mutate(timestamp = as.POSIXct(timestamp)) %>%
-      mutate(timestamp = format(timestamp, "%Y-%m-%d %H:%M")) %>%
-      mutate(timestamp2 = as.POSIXct(timestamp)) %>%
-      complete(timestamp2 = seq.POSIXt(as.POSIXct(start_date), as.POSIXct(end_date), by = "min")) %>%
-      mutate(date = as.Date(timestamp2)) %>%
-      mutate(hour = hour(ymd_hms(timestamp2))) %>%
-      mutate(monitor = device) %>%
-      select(monitor, everything())
+      
+      # Code to fill in the missing time gaps
+      mutate(timestamp = as.POSIXct(timestamp)) %>% #reformat for time manipulation
+      mutate(timestamp = format(timestamp, "%Y-%m-%d %H:%M")) %>% #remove seconds from timestamp
+      mutate(timestamp2 = as.POSIXct(timestamp)) %>% #reformat for time manipulation
+      complete(timestamp2 = seq.POSIXt(as.POSIXct(start_date), as.POSIXct(end_date), by = "min")) %>% #timestamp2 contains every minute intervals between the specified start date and end date
+      mutate(date = as.Date(timestamp2)) %>% #extract date from timestamp2
+      mutate(hour = hour(ymd_hms(timestamp2))) %>% #get the hour number too
+      mutate(monitor = device) %>% #add the monitor name to the data frame
+      select(monitor, everything()) #reorder the dataframe so the monitor is first (for table aesthetic)
     
     return(result_df_device)
   })
