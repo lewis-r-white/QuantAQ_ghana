2fdfe731752f7bcd884d2ccb096c63e45bed1c9a
lewis-r-white <lewisrwhite@gmail.com>
2024-11-11 13:37:38 -0800

    make loading data more modular

--- src/load_pollution_datasets.R
+++ src/load_pollution_datasets.R
@@ -1,10 +1,17 @@
-load_pollution_datasets <- function(pollutant) {
+load_pollution_datasets <- function(pollutant, file_path, file_type = "csv") {
   columns <- c("monitor", "timestamp", "date", "hour", pollutant)
   
-  data <- fread("/Users/lewiswhite/CHAP_columbia/QuantAQ/data/cloud/ghana_AQ_parent_full_20240925.csv", 
-                select = columns, showProgress = TRUE)
+  # Load data based on the specified file type
+  data <- switch(file_type,
+                 "csv" = fread(file_path, select = columns, showProgress = TRUE),
+                 "rds" = {
+                   data <- readRDS(file_path)
+                   data %>% select(any_of(columns))  # Select columns if they exist
+                 },
+                 stop("Unsupported file type. Use 'csv' or 'rds'.")
+  )
   
-  # Filter out rows with pm10 values above 1500 if the pollutant is pm10. Suggested data error entry. 
+  # Apply specific filtering if the pollutant is pm10
   if (pollutant == "pm10") {
     data <- data %>% filter(!!sym(pollutant) <= 1500)
   }
@@ -13,13 +20,12 @@ load_pollution_datasets <- function(pollutant) {
   data <- data %>% 
     mutate(source = if_else(!is.na(!!sym(pollutant)), "cloud", NA_character_))
   
+  # Split data into colocation and community datasets
   colocation_data <- data %>% filter(date >= as.Date("2023-08-16") & date <= as.Date("2023-09-20"))
-  
   community_data <- data %>% filter(date >= as.Date("2023-09-26"))
   
+  # Assign data to global environment with dynamic names
   assign(paste0(pollutant, "_raw"), data, envir = .GlobalEnv)
   assign(paste0(pollutant, "_colocation"), colocation_data, envir = .GlobalEnv)
   assign(paste0(pollutant, "_community"), community_data, envir = .GlobalEnv)
 }
-
-
